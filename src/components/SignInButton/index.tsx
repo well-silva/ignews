db408d1dfb89a83bab20075d5f0e8944b74ed103
Wellington Silva <wellington.silva@captalys.com.br>
2021-11-17 19:47:52 -0300

    atualizando projeto

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,30 @@
 import { FaGithub } from 'react-icons/fa'; 
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from 'next-auth/client'
+
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}
     >
       <FaGithub color="#04d361" />
-      Wellington Silva
-      <FiX color="#737380" className={styles.closeIcon} />
+      {session.user.name}
+      <FiX 
+        color="#737380" 
+        className={styles.closeIcon} 
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
